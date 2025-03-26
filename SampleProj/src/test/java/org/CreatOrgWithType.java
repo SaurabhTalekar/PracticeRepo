b7873051fe1c96e57da7227b689722f756ff6a04
Saurabh <talekarsaurabh1@gmail.com>
2025-03-26 14:21:11 +0530

    Add CreateOrgType Test

---
+++ SampleProj/src/test/java/org/CreatOrgWithType.java
@@ -0,0 +1,15 @@
+package org;
+
+import org.testng.annotations.Test;
+
+public class CreatOrgWithType {
+	
+	@Test
+	public void createOrgType() {
+		
+		System.out.println("Execute org Type");
+	
+		
+	}
+
+}
