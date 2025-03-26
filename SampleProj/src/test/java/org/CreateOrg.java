8ffa9c5855cee975ab6e0c2020702d97ed4e4f68
Saurabh <talekarsaurabh1@gmail.com>
2025-03-26 15:04:42 +0530

    changes done by TL

--- SampleProj/src/test/java/org/CreateOrg.java
+++ SampleProj/src/test/java/org/CreateOrg.java
@@ -7,12 +7,11 @@ public class CreateOrg {
 	@Test
 	public void createOrg() {
 		System.out.println("Execute org");
-<<<<<<< HEAD
+
 		System.out.println("Modified program***");
-=======
+
 		System.out.println("Modified program by Sam Engg.");
 		System.out.println("Ele 1 by Sam Engg.");
->>>>>>> branch 'master' of https://github.com/SaurabhTalekar/PracticeRepo.git
 		
 	}
 	
