ae2d603320ba0cdee10e62c55d21acce544d063c
Saurabh <talekarsaurabh1@gmail.com>
2025-03-26 13:34:13 +0530

    Create an Contact module and CreateconTest

---
+++ SampleProj/src/test/java/contact/createContact.java
@@ -0,0 +1,12 @@
+package contact;
+
+import org.testng.annotations.Test;
+
+public class createContact {
+
+	@Test
+	public void createcon() {
+		System.out.println("******Execute contact********");
+		
+	}
+}
