59e5b82c4d9fd544762fdcc4bf945a05a1660cf3
Saurabh <talekarsaurabh1@gmail.com>
2025-03-26 17:52:02 +0530

    Added new class to local branch

---
+++ SampleProj/src/test/java/org/CreateOrgWithDate.java
@@ -0,0 +1,16 @@
+package org;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgWithDate {
+	
+	@Test
+	public void createOrgType() {
+		
+		System.out.println("Execute org Date prog");
+		
+		
+		
+	}
+
+}
