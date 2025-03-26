3a8e50a10ea710dce11f92b042d1ce53f398b9bf
Saurabh <talekarsaurabh1@gmail.com>
2025-03-26 14:37:45 +0530

    Ele 1  added by sam

--- SampleProj/src/test/java/org/CreateOrg.java
+++ SampleProj/src/test/java/org/CreateOrg.java
@@ -7,7 +7,8 @@ public class CreateOrg {
 	@Test
 	public void createOrg() {
 		System.out.println("Execute org");
-		System.out.println("Modified program");
+		System.out.println("Modified program by Sam Engg.");
+		System.out.println("Ele 1 by Sam Engg.");
 		
 	}
 	
